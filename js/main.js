19bd03d0f934bc068c9643ed80db62845f0e572f
lununes <luciana.mn@gmail.com>
2020-08-03 01:17:50 -0300

    Série 4 > Curso 2 > aulas 1 e 4
    Ajustes nas aulas 1 e 4

--- js/main.js
+++ js/main.js
@@ -137,4 +137,94 @@ jQuery(document).ready(function($) {
 
   // custom code
 
+  // Scroll on tabs in Mobile
+    var hidWidth;
+    var scrollBarWidths = 40;
+
+    var widthOfList = function () {
+      var itemsWidth = 0;
+      $('.list a').each(function () {
+        var itemWidth = $(this).outerWidth();
+        itemsWidth += itemWidth;
+      });
+      return itemsWidth;
+    };
+
+    var widthOfHidden = function () {
+      var ww = 0 - $('.wrapper').outerWidth();
+      var hw = (($('.wrapper').outerWidth()) - widthOfList() - getLeftPosi()) - scrollBarWidths;
+      var rp = $(document).width() - ($('.nav-item.nav-link').last().offset().left + $('.nav-item.nav-link').last()
+        .outerWidth());
+
+      if (ww > hw) {
+        //return ww;
+        return (rp > ww ? rp : ww);
+      } else {
+        //return hw;
+        return (rp > hw ? rp : hw);
+      }
+    };
+
+    var getLeftPosi = function () {
+
+      var ww = 0 - $('.wrapper').outerWidth();
+      var lp = $('.list').position().left;
+
+      if (ww > lp) {
+        return ww;
+      } else {
+        return lp;
+      }
+    };
+
+    var reAdjust = function () {
+
+      // check right pos of last nav item
+      var rp = $(document).width() - ($('.nav-item.nav-link').last().offset().left + $('.nav-item.nav-link').last()
+        .outerWidth());
+      if (($('.wrapper').outerWidth()) < widthOfList() && (rp < 0)) {
+        $('.scroller-right').show().css('display', 'flex');
+      } else {
+        $('.scroller-right').hide();
+      }
+
+      if (getLeftPosi() < 0) {
+        $('.scroller-left').show().css('display', 'flex');
+      } else {
+        $('.item').animate({
+          left: "-=" + getLeftPosi() + "px"
+        }, 'slow');
+        $('.scroller-left').hide();
+      }
+    }
+
+    reAdjust();
+
+    $(window).on('resize', function (e) {
+      reAdjust();
+    });
+
+    $('.scroller-right').click(function () {
+
+      $('.scroller-left').fadeIn('slow');
+      $('.scroller-right').fadeOut('slow');
+
+      $('.list').animate({
+        left: "+=" + widthOfHidden() + "px"
+      }, 'slow', function () {
+        reAdjust();
+      });
+    });
+
+    $('.scroller-left').click(function () {
+
+      $('.scroller-right').fadeIn('slow');
+      $('.scroller-left').fadeOut('slow');
+
+      $('.list').animate({
+        left: "-=" + getLeftPosi() + "px"
+      }, 'slow', function () {
+        reAdjust();
+      });
+    });
 });
